452cb7095fa2d4d487eef9fbee33d3aaec80cd35
Jordan Bice <jmbice@gmail.com>
2018-05-18 10:19:03 -0700

    Create stringify function that passes all test

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,39 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result;
+
+  if (typeof obj === 'object' && obj !== null) {
+    //non-empty arrays and objects
+    if (Array.isArray(obj)){
+      //for arrays specifically
+      result = '[';
+      for (var j = 0; j < obj.length; j++) {
+        result += stringifyJSON(obj[j]);
+        j < obj.length-1 ? result += ',' : null;
+      }
+      result += ']';
+    } else {
+      //for objects speficially
+      result = '{';
+      var count = 0;
+      for (var key in obj) {
+        if (obj[key] === undefined) {
+          null;
+        } else if (typeof obj[key] === 'function') {
+          null;
+        } else {
+          result += '"' + key + '":' + stringifyJSON(obj[key]);
+          Object.keys(obj).length-1 !== count ? result += ',' : null;
+          count++;
+        }
+      }
+        result += '}';
+    }
+  } else if (typeof obj === 'string') {
+    result = '"' + obj + '"';
+  } else {
+    result = '' + obj + '';
+  }
+  return result;
 };
